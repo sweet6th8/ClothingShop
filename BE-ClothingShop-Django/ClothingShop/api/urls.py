a6fc97c4547424f45c509ffbd195fbab8ed09d4d
Nie <165691316+NieNieNe@users.noreply.github.com>
2024-11-06 20:14:00 +0700

    Update urls.py

--- BE-ClothingShop-Django/ClothingShop/api/urls.py
+++ BE-ClothingShop-Django/ClothingShop/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet
 from rest_framework_nested import routers
 
+
+
+
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='Categories')
 router.register(r'products', ProductViewSet, basename='Products')
