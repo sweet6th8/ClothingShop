2f8f5f8f7b21c5ccfe569c7aa2dfe46904e8f84e
Lang Nguyen <thilang0608@gmail.com>
2024-11-22 21:29:48 +0700

    create admin dashboard

--- BE-ClothingShop-Django/ClothingShop/ClothingShop/settings.py
+++ BE-ClothingShop-Django/ClothingShop/ClothingShop/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ClothingShop',  
         'USER': 'root',      
-        'PASSWORD': 'tit2222004',  
+        'PASSWORD': '123456',  
         'HOST': 'localhost',     
-        'PORT': '3307',        
+        'PORT': '3306',        
     }
 }
 
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
 
--- BE-ClothingShop-Django/ClothingShop/clothing_shop/admin.py
+++ BE-ClothingShop-Django/ClothingShop/clothing_shop/admin.py
@@ -11,4 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_id', 'product_name', 'slug', 'price', 'stock', 'is_available', 'category')
     search_fields = ('product_name', 'slug')
     list_filter = ('is_available', 'category')
+# đã đăng ký rồi
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Product, ProductAdmin)
     
